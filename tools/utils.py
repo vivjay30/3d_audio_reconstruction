21fbbb5f0afb7625477df69f1e48f350a0b31644
Maruchi Kim <maruchi.kim@gmail.com>
2019-11-28 16:19:02 -0800

    Update output of spectrogram to 333 x 483
    - Added some comments explaining window size and hop length choices
    - Should maximize n_mels for more frequency resolution

--- tools/utils.py
+++ tools/utils.py
@@ -22,16 +22,25 @@ def read_file(filename, sample_rate=None, trim=False):
 
 def log_mel_spec_tfm(fname, sample_rate=None):
     x, sample_rate = read_file(fname, sample_rate=sample_rate)
-    
-    n_fft = 512
-    hop_length = 16
-    n_mels = 128
+
+
+    # window > full period of a 20hz sinusoid (50ms)
+    # 22050 * 0.05 ~= 1102.5
+    n_fft = 1120
+
+    # overlap windows by 75%
+    # n_fft * (1-0.75)
+    hop_length = int(0.25*n_fft)
+
+    # n bins on y_axis
+    n_mels = 333
+
     fmin = 20
-    fmax = sample_rate / 2 
-    
-    mel_spec_power = librosa.feature.melspectrogram(x, sr=sample_rate, n_fft=n_fft, 
-                                                    hop_length=hop_length, 
-                                                    n_mels=n_mels, power=2.0, 
+    fmax = sample_rate / 2
+
+    mel_spec_power = librosa.feature.melspectrogram(x, sr=sample_rate, n_fft=n_fft,
+                                                    hop_length=hop_length,
+                                                    n_mels=n_mels, power=2.0,
                                                     fmin=fmin, fmax=fmax)
     mel_spec_db = librosa.power_to_db(mel_spec_power, ref=np.max)
     return mel_spec_db
